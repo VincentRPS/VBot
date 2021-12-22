6b6eba09c6626451bba465d65c34f4fba9dabd2d
Vincent <vincentbusiness55@gmail.com>
2021-12-22 15:12:13 +0800

    Make cogs work

--- src/bot.py
+++ src/bot.py
@@ -8,21 +8,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 _log = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 bot = pincer.Client(token=os.getenv("TOKEN"), intents=Intents.all())
 
-# TODO; get working cogs
-"""
-for cog in load.glob("cogs/*.py"):
-    cog.replace("/", ".").replace("\\", ".")[:-3]"""
-    
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
+        bot.load_cog(f"cogs.{filename[:-3]}")
+        print(f"{filename[:-3]} Cog has loaded!")
+
 @bot.event
 async def on_ready():
     _log.info("Bot is now ready.")
-
-@command(description="The Ping of the bot") # tempoary until i get a way to load cogs.
-async def ping(ctx: MessageContext):
-    await ctx.send("Pong!")
 
 bot.start_loop() # big brain me did this and it worked? bot.run won't work.
--- src/cogs/info.py
+++ src/cogs/info.py
@@ -1,13 +1,15 @@
 import logging
 from pincer import *
-from pincer.objects import * # TODO; clear the import crisis here.
+from pincer.objects import *
 
 _log = logging.getLogger(__name__)
 
 class Info:
     """Info, /ping /info /help etc."""
-    @command(description="The Ping of the bot", guild="917761976141238282")
+
+    @command(description="The Ping of the bot")
     async def ping(self, ctx: MessageContext):
         await ctx.send("Pong!")
+        _log.info("Ping command was used")
 
 setup = Info
